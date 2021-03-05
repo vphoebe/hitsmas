41bf8bc0ed0f8cf539da9e0138df9c06a0a48a2e
vphoebe <nick@stylefire.xyz>
2021-03-04 17:21:42 -0800

    chore: add apex predator

---
+++ src/data/hitman3/apexPredator.ts
@@ -0,0 +1,37 @@
+import { MissionType } from "../../types";
+
+const apexPredator: MissionType = {
+  location: "Berlin",
+  title: "Apex Predator",
+  version: 3,
+  targets: ["Target 1", "Target 2", "Target 3", "Target 4", "Target 5"],
+  disguises: [
+    { name: "Number 6" },
+    { name: "Rave On Suit" },
+    { name: "Bartender" },
+    { name: "Biker" },
+    { name: "Club Crew" },
+    { name: "Club Security" },
+    { name: "Dealer" },
+    { name: "Delivery Guy" },
+    { name: "DJ" },
+    { name: "Florida Man" },
+    { name: "Rolf Hirschmüller" },
+    { name: "Technician" },
+  ],
+  weapons: [
+    { name: "Sniper Rifle" },
+    { name: "Battle Axe" },
+    { name: "Combat Knife" },
+    { name: "Concealable Knife" },
+    { name: "Garden Fork" },
+    { name: "Hobby Knife" },
+    { name: "Old Axe" },
+    { name: "Kitchen Knife" },
+    { name: "Scrap Sword" },
+    { name: "Scissors" },
+    { name: "Screwdriver" },
+  ],
+};
+
+export default apexPredator;
--- src/data/index.ts
+++ src/data/index.ts
@@ -3,12 +3,13 @@ import { DataType } from "../types";
 import showstopper from "./hitman1/showstopper";
 import finishLine from "./hitman2/finishLine";
 import threeHeadedSerpent from "./hitman2/threeHeadedSerpent";
+import apexPredator from "./hitman3/apexPredator";
 
 const data: DataType = {
   global: {
     weapons: globalWeapons,
   },
-  missions: [showstopper, finishLine, threeHeadedSerpent],
+  missions: [showstopper, finishLine, threeHeadedSerpent, apexPredator],
 };
 
 export default data;
