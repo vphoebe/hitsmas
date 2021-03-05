a75ee35d4e9264bd7b7fea4351bb9b5e1e6eba6a
vphoebe <nick@stylefire.xyz>
2021-03-04 17:05:46 -0800

    chore: add three headed serpent

---
+++ src/data/hitman2/threeHeadedSerpent.ts
@@ -0,0 +1,43 @@
+import { MissionType } from "../../types";
+
+const threeHeadedSerpent: MissionType = {
+  location: "Santa Fortuna",
+  title: "Three-Headed Serpent",
+  version: 2,
+  targets: ["Rico Delgado", "Andrea Martinez", "Jorge Franco"],
+  disguises: [
+    { name: "Casual Tourist" },
+    { name: "Barman" },
+    { name: "Chef" },
+    { name: "Coca Field Guard" },
+    { name: "Coca Field Worker" },
+    { name: "Construction Worker" },
+    { name: "Drug Lab Worker" },
+    { name: "Drummer" },
+    { name: "Elite Guard" },
+    { name: "Gardener" },
+    { name: "Hippie" },
+    { name: "Hippo Whisperer" },
+    { name: "Mansion Guard" },
+    { name: "Mansion Staff" },
+    { name: "Shaman" },
+    { name: "Street Soldier" },
+    { name: "Submarine Crew" },
+    { name: "Submarine Engineer" },
+    { name: "Tattooist" },
+  ],
+  weapons: [
+    { name: "Barber Razor" },
+    { name: "Battle Axe" },
+    { name: "Cleaver" },
+    { name: "Folding Knife" },
+    { name: "Letter Opener" },
+    { name: "Machete" },
+    { name: "Sacrificial Knife" },
+    { name: "Kitchen Knife" },
+    { name: "Scissors" },
+    { name: "Screwdriver" },
+  ],
+};
+
+export default threeHeadedSerpent;
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,14 +1,14 @@
 import globalWeapons from "./global/weapons";
+import { DataType } from "../types";
 import showstopper from "./hitman1/showstopper";
 import finishLine from "./hitman2/finishLine";
-
-import { DataType } from "../types";
+import threeHeadedSerpent from "./hitman2/threeHeadedSerpent";
 
 const data: DataType = {
   global: {
     weapons: globalWeapons,
   },
-  missions: [showstopper, finishLine],
+  missions: [showstopper, finishLine, threeHeadedSerpent],
 };
 
 export default data;
